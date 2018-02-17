464c3be73bc5c98c8bcdad7a02968c04cf44f004
bmh10 <bmhomer13@gmail.com>
2018-02-17 09:38:00 +0000

    Initial commit

---
+++ Main.hs
@@ -0,0 +1,96 @@
+module Main where
+
+import Graphics.Gloss
+import qualified Graphics.Gloss.Game as GG
+import Graphics.Gloss.Data.ViewPort
+import Graphics.Gloss.Interface.Pure.Game as G2
+import System.IO  
+import System.Random 
+import Control.Monad
+import Data.Fixed
+import Data.List
+import Data.Maybe
+
+fps = 20
+width = 800
+height = 500 + dashboardHeight -- 31 * 15
+dashboardHeight = 20
+offset = 100
+
+window = InWindow "Collision" (width, height) (offset, offset)
+background = black
+
+data LifeGame = Game
+  { 
+    particles :: [Particle],
+    paused :: Bool,
+    gen :: StdGen
+  } deriving Show 
+
+data Particle = Particle
+  {
+    pos :: (Int, Int),
+    vel :: (Int, Int),
+    mass :: Int,
+    radius :: Float,
+    col :: G2.Color
+  } deriving Show
+
+particle pos vel mass radius col = Particle { pos = pos, vel = vel, mass = mass, radius = radius, col = col }
+
+-- Rendering
+render :: LifeGame -> Picture 
+render g = pictures [renderParticles g, 
+                     renderDashboard g]
+
+renderDashboard :: LifeGame -> Picture
+renderDashboard g = G2.color white $ translate (-300) (-fromIntegral height/2 + 5) $ scale 0.1 0.1 $ text $ "Particles: " ++ show (length (particles g))
+
+renderParticles g = pictures $  map renderParticle (particles g)
+
+renderParticle :: Particle -> Picture
+renderParticle p
+ = translate x' y' $ G2.color (col p) $ circleSolid (radius p)
+  where
+    (x, y) = pos p
+    (x', y') = (fromIntegral x, fromIntegral y)
+   
+
+-- Event handling
+handleKeys :: Event -> LifeGame -> LifeGame
+handleKeys (EventKey (Char 'p') Down _ _) g = togglePaused g
+handleKeys _ game = game
+
+togglePaused g = g { paused   = not (paused g) }
+
+update :: Float -> LifeGame -> LifeGame
+update secs game
+ | (paused game) = game
+ | otherwise     = updateGame game
+
+updateGame g = g { particles = updateParticles (particles g) }
+
+updateParticles [] = []
+updateParticles (p:ps) = updateParticle p ++ updateParticles ps
+  where updateParticle p
+          | inRange p = [p { pos = add (pos p) (vel p) }]
+          | otherwise = []
+
+inRange p = -w <= x && x <= w && -h <= y && y <= h
+  where (x, y) = pos p
+        w = quot width 2
+        h = quot height 2
+
+add (a,b) (c,d) = (a+c,b+d)
+
+
+initGame = do 
+  stdGen <- newStdGen
+  let initialParticles = [particle (-100, 0) ( 5, 0) 1 10 blue,
+                          particle ( 100, 0) (-5, 0) 1 10 red]
+  let initialState = Game { paused = False, particles = initialParticles, gen = stdGen }
+  return initialState
+
+main = do
+  initialState <- initGame
+  play window background fps initialState render handleKeys update
---
+++ README.md
@@ -0,0 +1,8 @@
+haskell-collision
+--------------
+
+![screenshot](img/screenshot.png)
+
+TODO:
+ - 1d collision (elastic/inelastic)
+ - 2d collision (elastic/inelastic)
